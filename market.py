f1254ab29257573d8efc4969d9ca556b65905984
Alex Roan <alex.roan@hotmail.com>
2018-11-13 00:32:31 +0000

    Added sandbox url

--- market.py
+++ market.py
@@ -3,6 +3,7 @@ from endpoint_type import Cryptocurrency
 class Market:
 
     __BASE_URL = 'https://pro-api.coinmarketcap.com'
+    __SANDBOX_URL = 'https://sandbox-api.coinmarketcap.com'
 
     def __init__(self, api_key):
         self.api_key = api_key
